7cdf54fd243240e3e896ab91b308bda94d12f8a5
dingpeihua <dph01953558@alibaba-inc.com>
2025-03-27 18:19:49 +0800

    增加功能

--- shared/src/commonMain/kotlin/com/peihua/chatbox/shared/compose/settings/tabs/model/ModelSettingsScreen.kt
+++ shared/src/commonMain/kotlin/com/peihua/chatbox/shared/compose/settings/tabs/model/ModelSettingsScreen.kt
@@ -4,8 +4,10 @@ package com.peihua.chatbox.shared.compose.settings.tabs.model
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DropdownMenuItem
@@ -21,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.unit.dp
 import chatboxcompose.shared.generated.resources.Res
 import chatboxcompose.shared.generated.resources.language
 import chatboxcompose.shared.generated.resources.settingsModelProvider
@@ -89,6 +92,7 @@ fun ModelSettingsScreen(modifier: Modifier = Modifier) {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             value = apiKey.value,
             onValueChange = {
@@ -99,6 +103,7 @@ fun ModelSettingsScreen(modifier: Modifier = Modifier) {
             textStyle = MaterialTheme.typography.labelMedium,
             modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             value = hostState.value,
             onValueChange = {
